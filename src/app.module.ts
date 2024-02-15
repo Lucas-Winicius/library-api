1fed43581d7e233c359f8eb09615961be24f309e
Lucas-Winicius <lucasclucas7@gmail.com>
2024-02-15 18:41:01 -0300

    authorization middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { BooksModule } from './modules/books/books.module';
+import { AuthMiddleware } from './modules/books/books.middleware';
 
 @Module({
   imports: [UsersModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: 'books', method: RequestMethod.POST });
+  }
+}
---
+++ src/modules/books/books.middleware.ts
@@ -0,0 +1,22 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private jwt: JwtService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const userCookie = req.headers.authorization.split(' ')[1];
+
+    try {
+      await this.jwt.verifyAsync(userCookie);
+      next();
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+}
