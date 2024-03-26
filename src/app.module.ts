58d087a088ede040fe7ea964218f0d1f882ecd57
Lucas-Winicius <lucasclucas7@gmail.com>
2024-03-26 00:37:45 -0300

    authentication when performing important user actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { UsersModule } from './modules/users/users.module';
 import { BooksModule } from './modules/books/books.module';
 import { AuthMiddleware } from './modules/books/books.middleware';
 import { PrismaService } from './services/prisma.service';
+import { AuthMiddlewareUser } from './modules/users/users.middleware';
 
 @Module({
   imports: [UsersModule, BooksModule],
@@ -25,5 +26,15 @@ export class AppModule implements NestModule {
         { path: 'books/(.*)', method: RequestMethod.GET },
       )
       .forRoutes('books');
+
+    consumer
+      .apply(AuthMiddlewareUser)
+      .exclude(
+        { path: 'users', method: RequestMethod.GET },
+        { path: 'users/(.*)', method: RequestMethod.GET },
+        { path: 'users/signup', method: RequestMethod.POST },
+        { path: 'users/signin', method: RequestMethod.POST },
+      )
+      .forRoutes('users');
   }
 }
---
+++ src/modules/users/users.middleware.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response, NextFunction } from 'express';
+import { PrismaService } from 'src/services/prisma.service';
+
+@Injectable()
+export class AuthMiddlewareUser implements NestMiddleware {
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    try {
+      const userEditedId = req.url.split('/')[1];
+      const userCookie = req.headers.authorization.split(' ')[1];
+      const token = await this.jwt.verifyAsync(userCookie);
+      const user = await this.prisma.user.findUnique({
+        where: { id: token.id },
+      });
+
+      if (user.admin || user.id !== userEditedId)
+        throw new UnauthorizedException();
+
+      next();
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+}
